09e3867725ca2e999a653c11cfadb457120b98ea
juliano <jaspart@gmail.com>
2015-10-12 18:02:04 -0500

    Registro Usuarios

---
+++ www/js/pap.js
@@ -0,0 +1,36 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+document.write("<script type='text/javascript' src='util.js'></script>");
+var usuario = {
+    nombre: null, apellido: null, correo: null, edad: 0, fechaNacimiento: null, codigo: null, clave: null, claveConfirmacion: null
+};
+
+function registrarUsuario() {
+    usuario.nombre = document.getElementById("nombreUsuario").value;
+    usuario.apellido = document.getElementById("apellidoUsuario").value;
+    usuario.correo = document.getElementById("correoUsuario").value;
+
+    $.ajax({
+        url: servicio + 'generic/post/usuario',
+        type: 'POST',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify(usuario),
+        success: function (resp) {
+            //función cargar cupones usuario.
+            alert('Usuario registrado')
+        },
+        error: function (e) {
+            var mensaje = message(e);
+            if (mensaje == null) {
+                mensajeSoporte();
+            } else {
+                alert(mensaje);
+            }
+        }
+    });
+}
---
+++ www/js/util.js
@@ -0,0 +1,21 @@
+var servicio = "http://http://190.14.226.155:8095/paprs/webresources/";
+//var servicio = "http://localhost:9091/webresources/";
+
+function message(e) {
+    var html = $.parseHTML(e.responseText)
+    if (html != null) {
+        for (var i = 0; i < html.length; i++) {
+            if (html[i].innerHTML != null && html[i].innerHTML.indexOf('java.lang') !== -1) {
+                return html[i].innerHTML.split(":")[1];
+            }
+        }
+    }
+}
+
+function mensajeSoporte() {
+    alert('Error no controlado, contactenos para asistirte o intenta de nuevo.');
+}
+
+function status() {
+    return "<center><img src='./img/spinner.gif'/></center>";
+}
