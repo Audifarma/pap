2e7d6b31fc9317b0246ba12173862ff34e646e46
juliano <juliano@INGSI12>
2015-10-14 08:49:28 -0500

    Registro Usuario

--- www/js/pap.js
+++ www/js/pap.js
@@ -7,7 +7,7 @@
 document.write("<script type='text/javascript' src='util.js'></script>");
 
 var usuario = {
-    codTipoDocumento: 0, documentoIdentidad: null, nombre: null, apellido: null, correo: null, edad: 0, fechaNacimiento: null, codigo: null, clave: null, claveConfirmacion: null
+    codTipoDocumento: 0, documentoIdentidad: null, nombre: null, apellido: null, correo: null, edad: 0, fechaNacimiento: null, codigo: null, clave: null, claveConfirmacion: null, sexo: null
 };
 
 function registrarUsuario() {
@@ -18,7 +18,14 @@ function registrarUsuario() {
     usuario.correo = document.getElementById("correoUsuario").value;
     usuario.clave = document.getElementById("claveUsuario").value;
     usuario.claveConfirmacion = document.getElementById("claveConfirmacionUsuario").value;
-
+    if ($('#radio1:checked').val() !== 'undefined') {
+        usuario.sexo = $('#radio1:checked').val();
+    }
+    if ($('#radio2:checked').val() !== 'undefined') {
+        usuario.sexo = $('#radio2:checked').val();
+    }
+    alert($('#radio1:checked').val());
+    alert($('#radio2:checked').val());
     $.ajax({
         url: servicio + 'generic/post/usuario',
         type: 'POST',
