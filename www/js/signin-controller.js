7574b3657bdfab9674807ee17fe0952a559c8733
juliano <jaspart@gmail.com>
2015-11-06 18:35:39 -0500

    Registro Usuarios

--- www/js/signin-controller.js
+++ www/js/signin-controller.js
@@ -1,4 +1,4 @@
-﻿var pap = pap || {};
+var pap = pap || {};
 
 pap.SignInController = function () {
 
@@ -30,7 +30,7 @@ pap.SignInController.prototype.emailAddressIsValid = function (email) {
 pap.SignInController.prototype.resetSignInForm = function () {
 
     var invisibleStyle = "bi-invisible",
-        invalidInputStyle = "bi-invalid-input";
+            invalidInputStyle = "bi-invalid-input";
 
     this.$ctnErr.html("");
     this.$ctnErr.removeClass().addClass(invisibleStyle);
@@ -44,14 +44,15 @@ pap.SignInController.prototype.resetSignInForm = function () {
 var usuario = {
     codTipoDocumento: 0, documentoIdentidad: null, nombre: null, apellido: null, correo: null, edad: 0, fechaNacimiento: null, codigo: null, clave: null, claveConfirmacion: null, sexo: null
 };
+
 pap.SignInController.prototype.onSignInCommand = function () {
 
     var me = this,
-        emailAddress = me.$txtdocumento.val().trim(),
-        password = me.$txtPassword.val().trim(),
-        invalidInput = false,
-        invisibleStyle = "bi-invisible",
-        invalidInputStyle = "bi-invalid-input";
+            emailAddress = me.$txtdocumento.val().trim(),
+            password = me.$txtPassword.val().trim(),
+            invalidInput = false,
+            invisibleStyle = "bi-invisible",
+            invalidInputStyle = "bi-invalid-input";
 
     // Reset styles.
     me.$ctnErr.removeClass().addClass(invisibleStyle);
@@ -81,7 +82,7 @@ pap.SignInController.prototype.onSignInCommand = function () {
 //        me.$txtdocumento.addClass(invalidInputStyle);
 //        return;
 //    }
-    usuario.documentoIdentidad = emailAddress ;    
+    usuario.documentoIdentidad = emailAddress;
     usuario.clave = password;
     $.mobile.loading("show");
 
@@ -90,7 +91,7 @@ pap.SignInController.prototype.onSignInCommand = function () {
         url: pap.Settings.signInUrl,
         dataType: 'json',
         contentType: 'application/json',
-        data: JSON.stringify(usuario),//"email=" + emailAddress + "&password=" + password,
+        data: JSON.stringify(usuario), //"email=" + emailAddress + "&password=" + password,
         success: function (resp) {
             $.mobile.loading("hide");
 
@@ -104,7 +105,7 @@ pap.SignInController.prototype.onSignInCommand = function () {
                     userProfileModel: resp.nombre,
                     sessionId: resp.correo,
                     expirationDate: expirationDate,
-                    keepSignedIn:me.$chkKeepSignedIn.is(":checked")
+                    keepSignedIn: me.$chkKeepSignedIn.is(":checked")
                 });
                 // Go to main menu.
                 $.mobile.navigate(me.papPageId);
@@ -113,7 +114,7 @@ pap.SignInController.prototype.onSignInCommand = function () {
                 if (resp.extras.msg) {
                     switch (resp.extras.msg) {
                         case pap.ApiMessages.DB_ERROR:
-                        // TODO: Use a friendlier error message below.
+                            // TODO: Use a friendlier error message below.
                             me.$ctnErr.html("<p>Ocurrio un problema y no se ha podido iniciar sesión.  Por favor intentelo en unos minutos.</p>");
                             me.$ctnErr.addClass("bi-ctn-err").slideDown();
                             break;
@@ -135,4 +136,128 @@ pap.SignInController.prototype.onSignInCommand = function () {
             me.$ctnErr.addClass("bi-ctn-err").slideDown();
         }
     });
+};
+
+pap.RegistroUsuarioController = function () {
+    this.$divRegistrarUsuario = null;
+    this.$tipoDocumentoUsuario = null;
+    this.$documentoUsuario = null;
+    this.$nombreUsuario = null;
+    this.$apellidoUsuario = null;
+    this.$epsUsuario = null;
+    this.$fechaNacimientoUsuario = null;
+    this.$radio = null;
+    this.$departamentoUsuario = null;
+    this.$municipioUsuario = null;
+    this.$direccionUsuario = null;
+    this.$correoUsuario = null;
+    this.$telefonoUsuario = null;
+    this.$celularUsuario = null;
+    this.$claveUsuario = null;
+    this.$claveConfirmacionUsuario = null;
+};
+
+pap.RegistroUsuarioController.prototype.init = function () {
+    this.$divRegistrarUsuario = $("#divRegistrarUsuario");
+    this.$tipoDocumentoUsuario = $("#tipoDocumentoUsuario", this.$divRegistrarUsuario);
+    this.$documentoUsuario = $("#documentoUsuario", this.$divRegistrarUsuario);
+    this.$nombreUsuario = $("#nombreUsuario", this.$divRegistrarUsuario);
+    this.$apellidoUsuario = $("#apellidoUsuario", this.$divRegistrarUsuario);
+    this.$epsUsuario = $("#epsUsuario", this.$divRegistrarUsuario);
+    this.$fechaNacimientoUsuario = $("#fechaNacimientoUsuario", this.$divRegistrarUsuario);
+    this.$radio = $("#radio", this.$divRegistrarUsuario);
+    this.$departamentoUsuario = $("#departamentoUsuario", this.$divRegistrarUsuario);
+    this.$municipioUsuario = $("#municipioUsuario", this.$divRegistrarUsuario);
+    this.$direccionUsuario = $("#direccionUsuario", this.$divRegistrarUsuario);
+    this.$correoUsuario = $("#correoUsuario", this.$divRegistrarUsuario);
+    this.$telefonoUsuario = $("#telefonoUsuario", this.$divRegistrarUsuario);
+    this.$celularUsuario = $("#celularUsuario", this.$divRegistrarUsuario);
+    this.$claveUsuario = $("#claveUsuario", this.$divRegistrarUsuario);
+    this.$claveConfirmacionUsuario = $("#claveConfirmacionUsuario", this.$divRegistrarUsuario);
+};
+
+pap.RegistroUsuarioController.prototype.validarRegistroUsuario = function (usuario, solicitudRegistro) {
+    var invalidInput = false, invisibleStyle = "bi-invisible",
+            invalidInputStyle = "bi-invalid-input", invalidSelectStyle = "bi-invalid-select";
+
+    this.$tipoDocumentoUsuario.removeClass(invalidInputStyle);
+    this.$documentoUsuario.removeClass(invalidInputStyle);
+    this.$nombreUsuario.removeClass(invalidInputStyle);
+    this.$apellidoUsuario.removeClass(invalidInputStyle);
+    this.$epsUsuario.removeClass(invalidInputStyle);
+    this.$fechaNacimientoUsuario.removeClass(invalidInputStyle);
+    this.$departamentoUsuario.removeClass(invalidInputStyle);
+    this.$municipioUsuario.removeClass(invalidInputStyle);
+    this.$direccionUsuario.removeClass(invalidInputStyle);
+    this.$correoUsuario.removeClass(invalidInputStyle);
+    this.$telefonoUsuario.removeClass(invalidInputStyle);
+    this.$celularUsuario.removeClass(invalidInputStyle);
+    this.$claveUsuario.removeClass(invalidInputStyle);
+    this.$claveConfirmacionUsuario.removeClass(invalidInputStyle);
+
+    if (usuario.codTipoDocumento === -1 || usuario.codTipoDocumento.length === 0) {
+        this.$tipoDocumentoUsuario.addClass(invalidInputStyle);
+        invalidInput = true;
+    }
+    if (usuario.documentoIdentidad.length === 0) {
+        this.$documentoUsuario.addClass(invalidInputStyle);
+        invalidInput = true;
+    }
+    if (usuario.nombre.length === 0) {
+        this.$nombreUsuario.addClass(invalidInputStyle);
+        invalidInput = true;
+    }
+    if (usuario.apellido.length === 0) {
+        this.$apellidoUsuario.addClass(invalidInputStyle);
+        invalidInput = true;
+    }
+    if (solicitudRegistro.codCliente == -1) {
+        this.$epsUsuario.addClass(invalidInputStyle);
+        invalidInput = true;
+    }
+    if (usuario.fechaNacimiento.length === 0) {
+        this.$fechaNacimientoUsuario.addClass(invalidInputStyle);
+        invalidInput = true;
+    }
+    if (solicitudRegistro.codDpto == -1) {
+        this.$departamentoUsuario.addClass(invalidInputStyle);
+        invalidInput = true;
+    }
+    if (solicitudRegistro.codCiudad == -1) {
+        this.$municipioUsuario.addClass(invalidInputStyle);
+        invalidInput = true;
+    }
+    if (usuario.direccion.length === 0) {
+        this.$direccionUsuario.addClass(invalidInputStyle);
+        invalidInput = true;
+    }
+    if (usuario.correo.length === 0) {
+        this.$correoUsuario.addClass(invalidInputStyle);
+        invalidInput = true;
+    }
+    if (usuario.telefono.length === 0) {
+        if (usuario.telefonoCelular.length === 0) {
+            this.$telefonoUsuario.addClass(invalidInputStyle);
+            invalidInput = true;
+        }
+    }
+    if (usuario.telefonoCelular.length === 0) {
+        if (usuario.telefono.length === 0) {
+            this.$celularUsuario.addClass(invalidInputStyle);
+            invalidInput = true;
+        }
+    }
+    if (usuario.clave.length === 0) {
+        this.$claveUsuario.addClass(invalidInputStyle);
+        invalidInput = true;
+    }
+    if (usuario.claveConfirmacion.length === 0) {
+        this.$claveConfirmacionUsuario.addClass(invalidInputStyle);
+        invalidInput = true;
+    }
+
+    this.$epsUsuario.selectmenu('refresh');
+    this.$departamentoUsuario.selectmenu('refresh');
+    this.$municipioUsuario.selectmenu('refresh');
+    return invalidInput;
 };
