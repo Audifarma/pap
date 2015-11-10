e95533d1f2f1bc075ce4653db908a55f1c884f5a
juliano <jaspart@gmail.com>
2015-11-09 19:33:07 -0500

    Registro Usuarios

--- www/js/settings.js
+++ www/js/settings.js
@@ -1,8 +1,12 @@
-﻿var pap = pap || {};
+var servicio = "http://190.14.226.155:8095/paprs/webresources/";
+//var servicio = "http://localhost:9090/webresources/";
+//var servicio = "http://localhost:8080/webresources/";
+
+var pap = pap || {};
 pap.Settings = pap.Settings || {};
 pap.Settings.signUpUrl = "http://127.0.0.1:30000/api/account/register";  //"http://192.168.1.104:30000/api/account/register"; //;
 //pap.Settings.signInUrl = "http://190.14.226.155:8095/paprs/webresources/generic/post/validarUsuario";//"http://127.0.0.1:30000/api/account/logon"; //"http://192.168.1.104:30000/api/account/logon"; //
-pap.Settings.signInUrl = "http://localhost:8080/webresources/generic/post/validarUsuario";
-pap.Settings.bookingsUrl = "http://localhost:9090/webresources/generic/post/validarUsuario";//"http://127.0.0.1:30000/api/bookings"; //"http://192.168.1.104:30000/api/bookings"; //
+pap.Settings.signInUrl = servicio + "generic/post/validarUsuario";//"http://localhost:8080/webresources/generic/post/validarUsuario";
+pap.Settings.bookingsUrl = servicio + "generic/post/validarUsuario";//"http://localhost:9090/webresources/generic/post/validarUsuario";//"http://127.0.0.1:30000/api/bookings"; //"http://192.168.1.104:30000/api/bookings"; //
 pap.Settings.sessionIdKey = "pap-session";
 pap.Settings.sessionTimeoutInMSec = 86400000 * 30;   // 30 days.
