3d8765b362e8b20a8e084f738a2ba8560af5ff27
juliano <juliano@INGSI12>
2015-11-23 11:39:11 -0500

    Consulta Autorizaciones Usuario

--- www/js/settings.js
+++ www/js/settings.js
@@ -1,4 +1,13 @@
 var servicio = "http://190.14.226.155:8095/paprs/webresources/";
+
+var usuario = {
+    codTipoDocumento: 0, documentoIdentidad: null, nombre: null, apellido: null, correo: null, edad: 0, fechaNacimiento: null, codigo: null,
+    clave: null, claveConfirmacion: null, sexo: null, direccion: null, telefono: null, telefonoCelular: null
+};
+
+var solicitudRegistro = {
+    codSolicitudRegistro: 0, codCliente: 0, codDpto: 0, codCiudad: 0, fechaRegistro: null, horaRegistro: null
+};
 //var servicio = "http://10.1.1.185:8095/paprs/webresources/";
 //var servicio = "http://localhost:9090/webresources/";
 //var servicio = "http://localhost:8080/webresources/";
@@ -11,3 +20,6 @@ pap.Settings.signInUrl = servicio + "generic/post/validarUsuario";//"http://loca
 pap.Settings.bookingsUrl = servicio + "generic/post/validarUsuario";//"http://localhost:9090/webresources/generic/post/validarUsuario";//"http://127.0.0.1:30000/api/bookings"; //"http://192.168.1.104:30000/api/bookings"; //
 pap.Settings.sessionIdKey = "pap-session";
 pap.Settings.sessionTimeoutInMSec = 86400000 * 30;   // 30 days.
+pap.Settings.usuario = usuario;
+pap.Settings.solicitudRegistro = solicitudRegistro;
+
