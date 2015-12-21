11d22fc1689e393d2af7965452ab04e82cba2b12
juliano <juliano@INGSI12>
2015-12-21 09:47:42 -0500

    Respuesta informe de pruebas, formateo de campos.

--- www/js/settings.js
+++ www/js/settings.js
@@ -11,6 +11,7 @@ var solicitudRegistro = {
 //var servicio = "http://10.1.1.185:8095/paprs/webresources/";
 //var servicio = "http://localhost:9090/webresources/";
 //var servicio = "http://localhost:8080/webresources/";
+//var servicio = "http://localhost:8091/webresources/";
 
 var pap = pap || {};
 pap.Settings = pap.Settings || {};
