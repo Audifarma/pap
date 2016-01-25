3ab9fac30a629b9b470ff91b9f24a6ffa1e3eb95
juliano <juliano@INGSI12>
2016-01-25 16:46:12 -0500

    Confirmación de autorizaciones.

--- www/js/settings.js
+++ www/js/settings.js
@@ -24,10 +24,16 @@ var alistamiento = {
 
 var pap = pap || {};
 pap.Settings = pap.Settings || {};
-pap.Settings.signUpUrl = "http://127.0.0.1:30000/api/account/register";  //"http://192.168.1.104:30000/api/account/register"; //;
+pap.Settings.TYPE_POST = 'POST';
+pap.Settings.TYPE_GET = 'GET';
+pap.Settings.APPLICATION_JSON = 'application/json';
+pap.Settings.DATA_TYPE_JSON = 'json';
+
+//pap.Settings.signUpUrl = "http://127.0.0.1:30000/api/account/register";  //"http://192.168.1.104:30000/api/account/register"; //;
 //pap.Settings.signInUrl = "http://190.14.226.155:8095/paprs/webresources/generic/post/validarUsuario";//"http://127.0.0.1:30000/api/account/logon"; //"http://192.168.1.104:30000/api/account/logon"; //
-pap.Settings.signInUrl = servicio + "generic/post/validarUsuario";//"http://localhost:8080/webresources/generic/post/validarUsuario";
-pap.Settings.autorizacionUrl = servicio + "pap/get/autorizacionesUsuario";//"http://localhost:8080/webresources/generic/post/validarUsuario";
+pap.Settings.signInUrl = servicio + "generic/post/validarUsuario";
+pap.Settings.autorizacionUrl = servicio + "pap/get/autorizacionesUsuario";
+pap.Settings.confirmarAutorizacionUrl = servicio + 'pap/post/autorizacionesUsuario';
 pap.Settings.bookingsUrl = servicio + "generic/post/validarUsuario";//"http://localhost:9090/webresources/generic/post/validarUsuario";//"http://127.0.0.1:30000/api/bookings"; //"http://192.168.1.104:30000/api/bookings"; //
 pap.Settings.sessionIdKey = "pap-session";
 pap.Settings.sessionTimeoutInMSec = 86400000 * 30;   // 30 days.
